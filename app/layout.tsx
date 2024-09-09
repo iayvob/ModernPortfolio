c954f2e1e77b05cba62e8bb20af6550f5a3d5d8e
iayvob <iayvob@learnrithm.com>
2024-09-09 15:10:45 +0100

    update script Sychrounous

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,18 +22,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+      >
         <div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
-        <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="iayvobi" data-description="Support me on Buy me a coffee!" data-message="You gonna support me 😊" data-color="#5F7FFF" data-position="Left" data-x_margin="18" data-y_margin="18"></script>
+
+        {/* Asynchronous Script */}
+        <Script
+          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+          data-name="BMC-Widget"
+          data-id="iayvobi"
+          data-description="Support me on Buy me a coffee!"
+          data-message="You gonna support me 😊"
+          data-color="#5F7FFF"
+          data-position="Left"
+          data-x_margin="18"
+          data-y_margin="18"
+          strategy="lazyOnload"
+        />
+
         <ActiveSectionContextProvider>
           <Header />
           {children}
-          <Toaster position="top-right"/>
+          <Toaster position="top-right" />
           <Footer />
         </ActiveSectionContextProvider>
         <ThemeSwitch />
-        </body>
+      </body>
     </html>
   );
 }
